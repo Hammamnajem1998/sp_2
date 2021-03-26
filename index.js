69cc8c8c48f464ea9b7f39891197f11eed9027a8
Hammam <hhammamnajemm@gmail.com>
2021-03-26 16:45:27 +0200

    'location'

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.post('/signup', (req, res) =>{
     const {error, value} = signupSchema.validate({username: req.body.username, email: req.body.email, password: req.body.password});
     if (error) return res.status(400).json({error: error.message});
 
-    const sql1 = `INSERT INTO users (name, email, password) VALUES ('${req.body.username}', '${req.body.email}','${req.body.password}')`;
+    const sql1 = `INSERT INTO users (name, email, password, location) VALUES ('${req.body.username}', '${req.body.email}','${req.body.password}', ST_GeomFromText('POINT(${req.body.location.latitude} ${req.body.location.longitude})') );`;
     con.query(sql1, (err, result) =>{
         if (err) return res.status(400).json({error: err.sqlMessage});
         return res.status(200).json({message: "added", id: result.insertId});
