f96787a3236f2d9e7dc84ff3b4170587763906d6
Hammam <hhammamnajemm@gmail.com>
2021-05-02 20:54:56 +0300

    'f'

--- routs/queue/queue-routs.js
+++ routs/queue/queue-routs.js
@@ -49,8 +49,8 @@ app.post('/addToQueue', (req, res) =>{
         if (queues_array[req.body.shop_id] == null) queues_array[req.body.shop_id] = new Array();  
         queues_array[req.body.shop_id].push({
             customerID: 'none',
-            first_name: 'New ', 
-            last_name : 'Customer', 
+            first_name: req.body.user_first_name, 
+            last_name : req.body.user_last_name, 
             email : 'Waiting', 
             photo : ''
         });
