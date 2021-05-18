69742ca20a466a561f9cad2cc58f43bb04f779af
Hammam <hhammamnajemm@gmail.com>
2021-05-18 18:30:44 +0300

    's'

--- routs/shop/shop-routs.js
+++ routs/shop/shop-routs.js
@@ -84,9 +84,17 @@ app.post('/shopImage', (req, res) =>{
 // add Shop 
 app.post('/addShop', (req, res) =>{
 
-    const sql1 = `INSERT INTO shops (name, type, time_unit, open_at, close_at, user_id, location) 
-    VALUES ('${req.body.name}', '${req.body.type}', '${req.body.time_unit}', '${req.body.open_at}', '${req.body.close_at}', '${req.body.user_id}', 
-    ST_GeomFromText('POINT(${req.body.location.latitude} ${req.body.location.longitude})') );`;
+    const sql1 = `INSERT INTO shops (name, type, time_unit, open_at, close_at, rating, user_id, location) 
+    VALUES (
+        '${req.body.name}', 
+        '${req.body.type}', 
+        '${req.body.time_unit}', 
+        '${req.body.open_at}', 
+        '${req.body.close_at}', 
+        '0.0', 
+        ${req.body.user_id}', 
+        ST_GeomFromText('POINT(${req.body.location.latitude} ${req.body.location.longitude})') 
+    );`;
     
     con.query(sql1, (err, shop) =>{
         if (err) return res.status(400).json({error: err.sqlMessage});
@@ -101,8 +109,8 @@ app.post('/shopRate', (req, res) =>{
     const sql1 = `INSERT INTO rates (user, shop, rate) VALUES (${req.body.customer}, ${req.body.shop}, ${req.body.rating}) ;`;
     con.query(sql1, (err, like) =>{
         if (err) return res.status(400).json({error: err.sqlMessage});
-        //return res.status(200).json({message: like});
     });
+
     // summation and average rating of all rates to this shop
     var sum = 0;
     var avr_rate;
